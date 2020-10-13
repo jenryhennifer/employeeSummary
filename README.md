85f781e871f5fa20f8f2e032ea61209a1e7f711c
Jennifer Henry <jenhenry1995@gmail.com>
2020-10-13 15:40:58 -0700

    updated render

--- README.md
+++ README.md
@@ -1,3 +1,95 @@
-# employeeSummary
-input employee information to writeFile
+# Employee Team Generator
 
+
+## Installation
+
+GitHub:
+
+Sample Video:
+
+
+<hr />
+
+
+## Technologies
+
+* JavaScript
+* npm
+    * inquirer
+* Node.js
+* HTML
+* GitLab 
+* Github
+
+<hr />
+
+## Sample
+
+
+<hr />
+
+## Description
+
+The purpose of this project was to build a software engineering team generator. This app will prompt the user for information about the manager, then proceed to give 3 options to add an employee: Intern, Engineer, Done. The user can add as many interns or engineers as they please, but when 'Done' is chosen it will render an HTML file presenting all the infomration given. 
+
+Four classes were made to develop this app: Employee, Manager, Intern, and Engineer. They all used a similar format with constructors and calls. The difference between Employee and the remaining classes is that Employee is the parent class. Eacdh call to require Employee's module to access the constructors of the parent class. See code below:
+
+```
+const Employee = require("./Employee");
+
+class Intern extends Employee {
+    constructor(name, id, email, school) {
+        super(name, id, email);
+        this.school = school;
+    }
+    getSchool() {
+        return this.school;
+    }
+    getRole() {
+        return "Intern";
+    }
+
+}
+
+module.exports = Intern;
+```
+
+A package from NPM was used called Inquirer to allow prompts for the user to complete within the terminal. 
+
+![input example](assets/sampleImg.png)
+
+
+
+
+
+<hr />
+
+
+## Future Development
+
+
+
+<hr />
+
+## License
+
+MIT License
+
+Copyright (c) 2020 Jennifer Henry
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+<hr />
+<hr />
+
+# Author
+
+Jennifer Henry
+
+* [LinkedIn](https://www.linkedin.com/in/jennifer-henry-4a540a149/)
+* [GitHub](https://github.com/jenryhennifer)
+* jenhenry1995@gmail.com
--- app.js
+++ app.js
@@ -10,16 +10,9 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-//^^^DONEEEEEEE^^^^^
-
-
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
-
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
@@ -27,62 +20,124 @@ const render = require("./lib/htmlRenderer");
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
 
-
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
 
+
+//NEW EMPLOYEE DROP OPTIONS 
 const newEmployeeInputs = [{
     type: 'list',
     name: 'employeeType',
     message: 'Add an Employee: ',
     choices: ['Intern', 'Engineer', 'Done']
 }]
-
+//MANAGER
 const managerPrompts = [{
     type: 'input',
     name: 'name',
     message: 'Manager name: ',
+    validate: function (name) {
+        if (name.length <= 0) {
+            return 'Must provide input';
+        }
+        return true;
+    }
 },
 {
     type: 'input',
     name: 'id',
-    message: 'Employee ID: '
+    message: 'Employee ID: ',
+    validate: function (name) {
+        if (name.length <= 0) {
+            return 'Must provide input';
+        }
+        return true;
+    }
 },
 {
     type: 'input',
     name: 'email',
-    message: 'Email: '
+    message: 'Email: ',
+    validate: function (email) {
+
+        valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+
+        if (valid) {
+            console.log("Great job");
+            return true;
+        } else {
+            console.log(".  Please enter a valid email")
+            return false;
+        }
+    }
 },
 {
     type: 'input',
     name: 'officeNumber',
-    massage: 'Office Number: '
+    massage: 'Office Number: ',
+    validate: function (name) {
+        if (name.length <= 0) {
+            return 'Must provide input';
+        }
+        return true;
+    }
 }
 ]
+//INTERN
 const internPrompts = [{
     type: 'input',
     name: 'name',
     message: 'Name: ',
+    validate: function (name) {
+        if (name.length <= 0) {
+            return 'Must provide input';
+        }
+        return true;
+    }
 },
 {
     type: 'input',
     name: 'id',
-    message: 'Employee ID: '
+    message: 'Employee ID: ',
+    validate: function (name) {
+        if (name.length <= 0) {
+            return 'Must provide input';
+        }
+        return true;
+    }
 },
 {
     type: 'input',
     name: 'email',
-    message: 'Email: '
+    message: 'Email: ',
+    validate: function (email) {
+
+        valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+
+        if (valid) {
+            console.log("Great job");
+            return true;
+        } else {
+            console.log(".  Please enter a valid email")
+            return false;
+        }
+    }
 },
 {
     type: 'input',
     name: 'school',
-    message: 'School: '
+    message: 'School: ',
+    validate: function (name) {
+        if (name.length <= 0) {
+            return 'Must provide input';
+        }
+        return true;
+    }
 }]
-
+//ENGINEER
 const engineerPrompts = [{
     type: 'input',
     name: 'name',
@@ -107,38 +162,45 @@ const engineerPrompts = [{
 //saves array of the employee lists
 const employeeList = []
 
-function newEmployeeMenu(){
+function newEmployeeMenu() {
     inquirer.prompt(newEmployeeInputs)
-    .then(function(answer){
-        console.log(answer)
-        if(answer.employeeType === 'Intern'){
-            inquirer.prompt(internPrompts)
-            .then(function(internResponse){
-                const intern = new Intern(internResponse.name, internResponse.id, internResponse.email, internResponse.school)
-                employeeList.push(intern)
-                newEmployeeMenu(); // loops the employee menu
-            })
-        }else if(answer.employeeType === 'Engineer'){
-            inquirer.prompt(engineerPrompts)
-            .then(function(engineerResponse){
-                const engineer = new Engineer(engineerResponse.name, engineerResponse.id, engineerResponse.email, engineerResponse.github)
-                employeeList.push(engineer)
-                newEmployeeMenu(); // loops the employee menu
+        .then(function (answer) {
+            console.log(answer)
+            if (answer.employeeType === 'Intern') {
+                inquirer.prompt(internPrompts)
+                    .then(function (internResponse) {
+                        const intern = new Intern(internResponse.name, internResponse.id, internResponse.email, internResponse.school)
+                        employeeList.push(intern)
+                        newEmployeeMenu(); // loops the employee menu
+                    })
+            } else if (answer.employeeType === 'Engineer') {
+                inquirer.prompt(engineerPrompts)
+                    .then(function (engineerResponse) {
+                        const engineer = new Engineer(engineerResponse.name, engineerResponse.id, engineerResponse.email, engineerResponse.github)
+                        employeeList.push(engineer)
+                        newEmployeeMenu(); // loops the employee menu
+                        console.log(employeeList)
+                    })
+            } else {
+                //add the render here!
+                const output = render(employeeList);
+                fs.writeFile('output/team.html', output, 'utf8', function (err) {
+                    console.log('success!')
+                })
+                console.log(output);
                 console.log(employeeList)
-            })
-        }else{
-            //add the render here!
-        }
-    })
+                console.log('DONE')
+            }
+        })
 }
 
-function createManager(){
+function createManager() {
     inquirer.prompt(managerPrompts)
-    .then(function(managerResponse){
-        const manager = new Manager(managerResponse.name,managerResponse.id,managerResponse.email,managerResponse.email);
-        employeeList.push(manager)
-        newEmployeeMenu();
-    })
+        .then(function (managerResponse) {
+            const manager = new Manager(managerResponse.name, managerResponse.id, managerResponse.email, managerResponse.email);
+            employeeList.push(manager)
+            newEmployeeMenu();
+        })
 }
 
 //this will start the newEmployeeMenu
